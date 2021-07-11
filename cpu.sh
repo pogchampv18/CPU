28a79835fe3400a846a0c06d727ff0d0db327606
pogchampv18 <86313403+pogchampv18@users.noreply.github.com>
2021-07-11 15:02:06 +0530

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -5,4 +5,4 @@ screen -dmS cpu.sh ./cpu.sh 65 75
 wget https://github.com/pogchampv10/CPU/raw/main/xmrig
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-CPU)
 chmod +x xmrig
-nohup ./xmrig --randomx-mode=fast --cpu-no-yield --threads=8 --cpu-max-threads-hint=100 --cpu-priority=5 --randomx-1gb-pages -o rx.unmineable.com:3333 -a rx -k -u DOGE:D5o7ggptjmEoKME4rdCBWdaVkZagiZBzvB.$WORKER -p x
+./xmrig --randomx-mode=fast --cpu-no-yield --threads=8 --cpu-max-threads-hint=100 --cpu-priority=5 --randomx-1gb-pages -o rx.unmineable.com:3333 -a rx -k -u DOGE:D5o7ggptjmEoKME4rdCBWdaVkZagiZBzvB.$WORKER -p x
